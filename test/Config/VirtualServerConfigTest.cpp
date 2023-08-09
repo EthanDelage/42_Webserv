ed0208a8e91c32b1a69cd99c6c74edee27cfe7f5
mururoahh <ferraudhugo@gmail.com>
2023-08-09 08:42:01 +0200

    add more test for VirtualServerConfig parseServerName

--- test/Config/VirtualServerConfigTest.cpp
+++ test/Config/VirtualServerConfigTest.cpp
@@ -11,11 +11,31 @@
 /* ************************************************************************** */
 #include "VirtualServerConfigTest.hpp"
 
-TEST_F(VirtualServerConfigTest, testParseServerNameValid) {
+TEST_F(VirtualServerConfigTest, testParseSingleServerName) {
 	std::vector<std::string> serverNames;
 
 	serverNames.push_back("localhost");
 	EXPECT_EQ(parseServerNameTest("server_name localhost;"), serverNames);
+}
+
+TEST_F(VirtualServerConfigTest, testParseMultipleServerName) {
+	std::vector<std::string> serverNames;
+
+	serverNames.push_back("localhost");
+	serverNames.push_back("test");
+	serverNames.push_back("tost");
+	EXPECT_EQ(parseServerNameTest("server_name localhost test tost;"), serverNames);
+}
+
+TEST_F(VirtualServerConfigTest, testParseMultipleServerNameDirectives) {
+	std::vector<std::string> serverNames;
+
+	serverNames.push_back("localhost");
+	EXPECT_EQ(parseServerNameTest("server_name localhost;"), serverNames);
+	serverNames.push_back("test");
+	EXPECT_EQ(parseServerNameTest("server_name test;"), serverNames);
+	serverNames.push_back("tost");
+	EXPECT_EQ(parseServerNameTest("server_name tost;"), serverNames);
 }
 
 std::vector<std::string> VirtualServerConfigTest::parseServerNameTest(char* line) {
