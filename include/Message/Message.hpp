69118f99b3e05be541538f92adfcfea42534d219
= <ethandelage04@gmail.com>
2023-08-19 21:53:49 +0200

    Start message class

---
+++ include/Message/Message.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Message.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelage <edelage@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 16:11:00 by edelage           #+#    #+#             */
+/*   Updated: 2023/08/17 16:11:00 by edelage          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef MESSAGE_HPP
+# define MESSAGE_HPP
+
+# include <string>
+
+typedef struct httpVersion_s	httpVersion_t;
+struct httpVersion_s {
+	unsigned int	major;
+	unsigned int	minor;
+};
+
+class Message {
+
+private:
+	httpVersion_t	_httpVersion;
+
+public:
+	Message();
+	~Message();
+
+	std::string	httpVersionToString() const;
+
+};
+
+#endif
---
+++ src/server/message/Message.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Message.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelage <edelage@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 16:11:00 by edelage           #+#    #+#             */
+/*   Updated: 2023/08/17 16:11:00 by edelage          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#include "Message/Message.hpp"
+
+Message::Message() {
+	_httpVersion.major = 0;
+	_httpVersion.minor = 0;
+}
+
+Message::~Message() {}
+
+std::string Message::httpVersionToString() const {
+	return ("HTTP/" + std::to_string(_httpVersion.major) + '.' + std::to_string(_httpVersion.minor));
+}
