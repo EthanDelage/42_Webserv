b1952fc4849d7fc5dcfcc3a9e004018eecc67796
= <ethandelage04@gmail.com>
2023-10-20 02:35:46 +0200

    Add  handler

--- src/server/message/Header.cpp
+++ src/server/message/Header.cpp
@@ -110,7 +110,8 @@ bool Header::isValidHeader(std::string const & headerKey) {
 	std::string const	 validHeader[] = {
 		"Host",
 		"Content-Length",
-		"Accept"};
+		"Accept",
+		"Transfer-Encoding"};
 
 	for (size_t i = 0; i != sizeof(validHeader) / sizeof (*validHeader); ++i) {
 		if (headerKey == validHeader[i])
