8de3f1c3bedefcd5726daa145bdfd96588dab8f7
Ethan Delage <110034497+EthanDelage@users.noreply.github.com>
2023-10-24 00:27:54 +0200

    39 handle sig int signal (#40)
    * Add signal for SIGINT to quit webserv correctly
    
    * Request move on stack

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,32 @@
 #include "config/Config.hpp"
 #include "server/Server.hpp"
 #include <iostream>
+#include <csignal>
+
+static void closeServer(int signal);
 
 int	main(int argc, char** argv, char **envp) {
 	Config	config;
 	Server	server;
 
+	signal(SIGINT, &closeServer);
 	if (argc != 2) {
 		std::cerr << "Too few arguments" << std::endl;
 		return (1);
 	}
 	try {
 		config.parse(argv[1]);
-		server.init(config, envp);
-		server.listener(config);
+		server.init(&config, envp);
+		server.listener();
 	} catch (std::exception const & e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return (1);
 	}
+	std::cout << std::endl << "Webserv close" << std::endl;
+	return (0);
+}
+
+static void closeServer(int signal) {
+	(void) signal;
+	Server::setCloseServer();
 }
